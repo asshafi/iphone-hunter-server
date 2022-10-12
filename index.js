a56df60300bb411a9df6c90772ba442b25659b02
shafi <asshafi15@gmail.com>
2022-10-12 21:27:54 +0600

    node and express js practice with making api

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app=express();
+const phones= require('./phones.json')
+const port=5000;
+
+app.get('/',(req,res)=>{
+    res.send('Look mama!! Iphone server running')
+});
+
+app.get('/phones',(req,res)=>{
+    res.send(phones)
+});
+
+app.get('/phones/:id',(req,res)=>{
+    const id=parseInt(req.params.id);
+
+    const phone=phones.find(ph=>ph.id===id) ||{};
+    res.send(phone)
+});
+
+app.listen(port,()=>{
+    console.log(`Iphone server is running on port: ${port }`)
+})
